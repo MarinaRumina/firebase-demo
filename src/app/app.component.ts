cff4dbcc48865d2b79b92c0d48b58796538f9f28
Marina Rumyantsev <aniramrum@gmail.com>
2018-11-22 00:24:58 +0200

    Async Pipe/Reading an Object (npt working properly)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,22 @@
 import { Component, OnDestroy } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
-import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnDestroy{
-  courses: any[];
-  subscription: Subscription;
+export class AppComponent {
+  // $ at the end of var means it is observable
+  courses$: Observable<{}[]>;
+  course$: Observable<{}>;
+  author$;
+
 
   constructor(db: AngularFireDatabase) {
-    this.subscription = db.list('/courses').valueChanges().subscribe(courses => {
-      this.courses = courses;
-      console.log(this.courses);
-    });
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.courses$ = db.list('/courses').valueChanges();
+    this.course$ = db.object('/courses/1').valueChanges().pipe();
+    this.author$ = db.object('author/1');
   }
 }

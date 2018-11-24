e4b2186b0de8ddc7ecb9f0e8bbcb38bf1b5f1ffa
Marina Rumyantsev <aniramrum@gmail.com>
2018-11-25 00:54:45 +0200

    firebase crud operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,21 +9,45 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   // $ at the end of var means it is observable
-  coursesList$: AngularFireList<{}>;
+  dbCoursesList$: AngularFireList<{}>;
   courses$: Observable<{}>;
   // course$: Observable<{}>;
   author$: Observable<{}>;
   // db: AngularFireDatabase;
 
-  constructor(db: AngularFireDatabase) {
-    this.coursesList$ = db.list('/courses');
-    this.courses$ = this.coursesList$.valueChanges();
+  constructor(private db: AngularFireDatabase) {
+    this.dbCoursesList$ = db.list('/courses');
+    this.courses$ = this.dbCoursesList$.valueChanges();
     // this.course$ = db.object('/courses/1').valueChanges();
     this.author$ = db.object('/authors/1').valueChanges();
   }
 
   add(course: HTMLInputElement) {
-    this.coursesList$.push(course.value);
+    this.dbCoursesList$.push({
+      name: course.value,
+      price: 150,
+      isLive: true,
+      sections: [
+        { title: 'Components' },
+        { title: 'Directives' },
+        { title: 'Template' }
+      ]
+    });
     course.value = '';
+  }
+
+  update(course) {
+    this.db.object('/courses/' + course)
+      .set({
+        title: course + ' UPDATED',
+        price: 150
+      });
+  }
+
+  delete(course) {
+    this.db.object('/courses/' + course)
+      .remove()
+      .then(x => console.log('Deleted'))
+      .catch(err => console.log('Error'));
   }
 }
